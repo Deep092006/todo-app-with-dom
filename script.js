dfa21286900d683c87cdadb6bc0bba69c7d5fc9b
Your Name <you@example.com>
2025-01-01 16:20:11 +0530

    done project

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let task = document.querySelector(".task");
 let submit = document.querySelector("button");
 let list = document.querySelector("ul");
+let delclass = document.querySelectorAll(".delbtn")
+
 submit.addEventListener("click",function(e){
     console.log(task.value)
     let li = document.createElement("li");
     li.innerText = task.value;
     list.appendChild(li);
+    let delbtn = document.createElement("button");
+    delbtn.classList.add("delbtn");
+    delbtn.innerText = "delete";
+    li.appendChild(delbtn);
 });
+for(del of delclass){
+    del.addEventListener("click",function(e){
+        let par = this.parentElement;
+        console.log(par);
+        par.remove();
+    });
+}
